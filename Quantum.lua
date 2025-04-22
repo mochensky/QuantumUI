51c76e1da14e784cd6915ee40b7997751cfe45d7
mochensky <162050937+mochensky@users.noreply.github.com>
2025-04-22 19:43:38 +0300

    Update Quantum.lua

--- Quantum.lua
+++ Quantum.lua
@@ -106,7 +106,7 @@ function Quantum.new(config)
 
     self.searchBox = Instance.new("TextBox")
     self.searchBox.Name = "SearchBox"
-    self.searchBox.Size = UDim2.new(0.9, 0, 0.9, 0)
+    self.searchBox.Size = UDim2.new(1, 0, 1, 0)
     self.searchBox.Position = UDim2.new(0.05, 0, 0.1, 0)
     self.searchBox.BackgroundTransparency = 1
     self.searchBox.TextColor3 = self.searchTextColor
